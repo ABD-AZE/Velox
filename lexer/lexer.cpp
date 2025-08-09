5eb678d145cd0631554492f37e075be4c9b2817d
Nisarg Prajapati <147189996+Munnond@users.noreply.github.com>
2025-08-09 21:23:32 +0530

    Update lexer.cpp

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -9,105 +9,106 @@
 #include <algorithm>
 #include <unordered_map>
 
-static const std::unordered_map<TokenType, std::string> TokenTypeNames = {
-    {TokenType::WS, "WS"},
-    {TokenType::LINE_COMMENT, "LINE_COMMENT"},
-    {TokenType::BLOCK_COMMENT, "BLOCK_COMMENT"},
-    {TokenType::ELLIPSIS, "ELLIPSIS"},
-    {TokenType::COMPOUND_RIGHTSHIFT, "COMPOUND_RIGHTSHIFT"},
-    {TokenType::COMPOUND_LEFTSHIFT, "COMPOUND_LEFTSHIFT"},
-    {TokenType::COMPOUND_SUM, "COMPOUND_SUM"},
-    {TokenType::COMPOUND_DIFFERENCE, "COMPOUND_DIFFERENCE"},
-    {TokenType::COMPOUND_PRODUCT, "COMPOUND_PRODUCT"},
-    {TokenType::COMPOUND_DIVISION, "COMPOUND_DIVISION"},
-    {TokenType::COMPOUND_REMAINDER, "COMPOUND_REMAINDER"},
-    {TokenType::COMPOUND_AND, "COMPOUND_AND"},
-    {TokenType::COMPOUND_XOR, "COMPOUND_XOR"},
-    {TokenType::COMPOUND_OR, "COMPOUND_OR"},
-    {TokenType::EQUAL, "EQUAL"},
-    {TokenType::NOTEQUAL, "NOTEQUAL"},
-    {TokenType::LESSTHANEQUAL, "LESSTHANEQUAL"},
-    {TokenType::GREATERTHANEQUAL, "GREATERTHANEQUAL"},
-    {TokenType::INCREMENT_OPERATOR, "INCREMENT_OPERATOR"},
-    {TokenType::DECREMENT_OPERATOR, "DECREMENT_OPERATOR"},
-    {TokenType::ARROW_OPERATOR, "ARROW_OPERATOR"},
-    {TokenType::LEFT_SHIFT, "LEFT_SHIFT"},
-    {TokenType::RIGHT_SHIFT, "RIGHT_SHIFT"},
-    {TokenType::LAND, "LAND"},
-    {TokenType::LOR, "LOR"},
-    {TokenType::VOID, "VOID"},
-    {TokenType::RETURN, "RETURN"},
-    {TokenType::IF, "IF"},
-    {TokenType::ELSE, "ELSE"},
-    {TokenType::DO, "DO"},
-    {TokenType::WHILE, "WHILE"},
-    {TokenType::FOR, "FOR"},
-    {TokenType::BREAK, "BREAK"},
-    {TokenType::CONTINUE, "CONTINUE"},
-    {TokenType::STATIC, "STATIC"},
-    {TokenType::EXTERN, "EXTERN"},
-    {TokenType::INT, "INT"},
-    {TokenType::LONG, "LONG"},
-    {TokenType::SIGNED, "SIGNED"},
-    {TokenType::UNSIGNED, "UNSIGNED"},
-    {TokenType::DOUBLE, "DOUBLE"},
-    {TokenType::CHAR, "CHAR"},
-    {TokenType::SIZEOF, "SIZEOF"},
-    {TokenType::STRUCT, "STRUCT"},
-    {TokenType::GOTO, "GOTO"},
-    {TokenType::SWITCH, "SWITCH"},
-    {TokenType::CASE, "CASE"},
-    {TokenType::DEFAULT_CASE, "DEFAULT_CASE"},
-    {TokenType::PRINTF, "PRINTF"},
-    {TokenType::SCANF, "SCANF"},
-    {TokenType::TYPEDEF, "TYPEDEF"},
-    {TokenType::ENUM, "ENUM"},
-    {TokenType::UNION, "UNION"},
-    {TokenType::FLOAT_CONSTANT, "FLOAT_CONSTANT"},
-    {TokenType::ULONG_CONSTANT, "ULONG_CONSTANT"},
-    {TokenType::LONG_CONSTANT, "LONG_CONSTANT"},
-    {TokenType::UINT_CONSTANT, "UINT_CONSTANT"},
-    {TokenType::INT_CONSTANT, "INT_CONSTANT"},
-    {TokenType::CHARACTER, "CHARACTER"},
-    {TokenType::STRING, "STRING"},
-    {TokenType::IDENTIFIER, "IDENTIFIER"},
-    {TokenType::OPEN_PARENTHESES, "OPEN_PARENTHESES"},
-    {TokenType::CLOSE_PARENTHESES, "CLOSE_PARENTHESES"},
-    {TokenType::OPEN_BRACE, "OPEN_BRACE"},
-    {TokenType::CLOSE_BRACE, "CLOSE_BRACE"},
-    {TokenType::OPEN_BRACKET, "OPEN_BRACKET"},
-    {TokenType::CLOSE_BRACKET, "CLOSE_BRACKET"},
-    {TokenType::SEMICOLON, "SEMICOLON"},
-    {TokenType::COLON, "COLON"},
-    {TokenType::COMMA, "COMMA"},
-    {TokenType::DOT, "DOT"},
-    {TokenType::QUESTION_MARK, "QUESTION_MARK"},
-    {TokenType::ASSIGNMENT, "ASSIGNMENT"},
-    {TokenType::PLUS, "PLUS"},
-    {TokenType::HYPHEN, "HYPHEN"},
-    {TokenType::ASTERISK, "ASTERISK"},
-    {TokenType::FORWARD_SLASH, "FORWARD_SLASH"},
-    {TokenType::PERCENT_SIGN, "PERCENT_SIGN"},
-    {TokenType::TILDE, "TILDE"},
-    {TokenType::NOT, "NOT"},
-    {TokenType::AMP, "AMP"},
-    {TokenType::LESSTHAN, "LESSTHAN"},
-    {TokenType::GREATERTHAN, "GREATERTHAN"},
-    {TokenType::AAND, "AAND"},
-    {TokenType::AOR, "AOR"},
-    {TokenType::XOR, "XOR"},
+static const std::unordered_map<TokenType,std::string> TokenTypeNames={
+    {TokenType::WS,"WS"},
+    {TokenType::LINE_COMMENT,"LINE_COMMENT"},
+    {TokenType::BLOCK_COMMENT,"BLOCK_COMMENT"},
+    {TokenType::ELLIPSIS,"ELLIPSIS"},
+    {TokenType::COMPOUND_RIGHTSHIFT,"COMPOUND_RIGHTSHIFT"},
+    {TokenType::COMPOUND_LEFTSHIFT,"COMPOUND_LEFTSHIFT"},
+    {TokenType::COMPOUND_SUM,"COMPOUND_SUM"},
+    {TokenType::COMPOUND_DIFFERENCE,"COMPOUND_DIFFERENCE"},
+    {TokenType::COMPOUND_PRODUCT,"COMPOUND_PRODUCT"},
+    {TokenType::COMPOUND_DIVISION,"COMPOUND_DIVISION"},
+    {TokenType::COMPOUND_REMAINDER,"COMPOUND_REMAINDER"},
+    {TokenType::COMPOUND_AND,"COMPOUND_AND"},
+    {TokenType::COMPOUND_XOR,"COMPOUND_XOR"},
+    {TokenType::COMPOUND_OR,"COMPOUND_OR"},
+    {TokenType::EQUAL,"EQUAL"},
+    {TokenType::NOTEQUAL,"NOTEQUAL"},
+    {TokenType::LESSTHANEQUAL,"LESSTHANEQUAL"},
+    {TokenType::GREATERTHANEQUAL,"GREATERTHANEQUAL"},
+    {TokenType::INCREMENT_OPERATOR,"INCREMENT_OPERATOR"},
+    {TokenType::DECREMENT_OPERATOR,"DECREMENT_OPERATOR"},
+    {TokenType::ARROW_OPERATOR,"ARROW_OPERATOR"},
+    {TokenType::LEFT_SHIFT,"LEFT_SHIFT"},
+    {TokenType::RIGHT_SHIFT,"RIGHT_SHIFT"},
+    {TokenType::LAND,"LAND"},
+    {TokenType::LOR,"LOR"},
+    {TokenType::VOID,"VOID"},
+    {TokenType::RETURN,"RETURN"},
+    {TokenType::IF,"IF"},
+    {TokenType::ELSE,"ELSE"},
+    {TokenType::DO,"DO"},
+    {TokenType::WHILE,"WHILE"},
+    {TokenType::FOR,"FOR"},
+    {TokenType::BREAK,"BREAK"},
+    {TokenType::CONTINUE,"CONTINUE"},
+    {TokenType::STATIC,"STATIC"},
+    {TokenType::EXTERN,"EXTERN"},
+    {TokenType::INT,"INT"},
+    {TokenType::LONG,"LONG"},
+    {TokenType::SIGNED,"SIGNED"},
+    {TokenType::UNSIGNED,"UNSIGNED"},
+    {TokenType::DOUBLE,"DOUBLE"},
+    {TokenType::CHAR,"CHAR"},
+    {TokenType::SIZEOF,"SIZEOF"},
+    {TokenType::STRUCT,"STRUCT"},
+    {TokenType::GOTO,"GOTO"},
+    {TokenType::SWITCH,"SWITCH"},
+    {TokenType::CASE,"CASE"},
+    {TokenType::DEFAULT_CASE,"DEFAULT_CASE"},
+    {TokenType::PRINTF,"PRINTF"},
+    {TokenType::SCANF,"SCANF"},
+    {TokenType::TYPEDEF,"TYPEDEF"},
+    {TokenType::ENUM,"ENUM"},
+    {TokenType::UNION,"UNION"},
+    {TokenType::FLOAT_CONSTANT,"FLOAT_CONSTANT"},
+    {TokenType::ULONG_CONSTANT,"ULONG_CONSTANT"},
+    {TokenType::LONG_CONSTANT,"LONG_CONSTANT"},
+    {TokenType::UINT_CONSTANT,"UINT_CONSTANT"},
+    {TokenType::INT_CONSTANT,"INT_CONSTANT"},
+    {TokenType::CHARACTER,"CHARACTER"},
+    {TokenType::STRING,"STRING"},
+    {TokenType::IDENTIFIER,"IDENTIFIER"},
+    {TokenType::OPEN_PARENTHESES,"OPEN_PARENTHESES"},
+    {TokenType::CLOSE_PARENTHESES,"CLOSE_PARENTHESES"},
+    {TokenType::OPEN_BRACE,"OPEN_BRACE"},
+    {TokenType::CLOSE_BRACE,"CLOSE_BRACE"},
+    {TokenType::OPEN_BRACKET,"OPEN_BRACKET"},
+    {TokenType::CLOSE_BRACKET,"CLOSE_BRACKET"},
+    {TokenType::SEMICOLON,"SEMICOLON"},
+    {TokenType::COLON,"COLON"},
+    {TokenType::COMMA,"COMMA"},
+    {TokenType::DOT,"DOT"},
+    {TokenType::QUESTION_MARK,"QUESTION_MARK"},
+    {TokenType::ASSIGNMENT,"ASSIGNMENT"},
+    {TokenType::PLUS,"PLUS"},
+    {TokenType::HYPHEN,"HYPHEN"},
+    {TokenType::ASTERISK,"ASTERISK"},
+    {TokenType::FORWARD_SLASH,"FORWARD_SLASH"},
+    {TokenType::PERCENT_SIGN,"PERCENT_SIGN"},
+    {TokenType::TILDE,"TILDE"},
+    {TokenType::NOT,"NOT"},
+    {TokenType::AMP,"AMP"},
+    {TokenType::LESSTHAN,"LESSTHAN"},
+    {TokenType::GREATERTHAN,"GREATERTHAN"},
+    {TokenType::AAND,"AAND"},
+    {TokenType::AOR,"AOR"},
+    {TokenType::XOR,"XOR"},
 };
 
 static std::string TokenTypeToString(TokenType type) {
-    if (auto it = TokenTypeNames.find(type); it != TokenTypeNames.end())
+    if(auto it=TokenTypeNames.find(type);it!=TokenTypeNames.end()){
         return it->second;
+    }
     return "UNKNOWN_TOKEN";
 }
 
-Lexer::Lexer(const std::string &file) : inputFileStream(file) {
+Lexer::Lexer(const std::string &file):inputFileStream(file){
   inputFileStream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-  if (!inputFileStream.is_open()) {
-    throw std::runtime_error("Failed to open file: " + file);
+  if(!inputFileStream.is_open()){
+    throw std::runtime_error("Failed to open file: "+file);
   }
 }
 
@@ -144,32 +145,33 @@ void Lexer::PrintTokens() const {
   }
 }
 
-void Lexer::PrintErrors() const {
+void Lexer::PrintErrors() const{
     using namespace termcolor;
 
-    if (errors.empty()) {
-        std::cout << green << "[ok] No lexical errors.\n" << reset;
+    if(errors.empty()){
+        std::cout<<green<<"[ok] No lexical errors.\n"<<reset;
         return;
     }
 
-    std::cout << bold << red << "[errors] Lexical errors found: "
-              << errors.size() << "\n" << reset;
+    std::cout<<bold<<red<<"[errors] Lexical errors found: "
+            <<errors.size()<<"\n"<<reset;
 
-    for (const auto& e : errors) {
-        std::cout << red
-                  << std::format("  {}:{}: unexpected token '{}'\n",
+    for(const auto& e:errors){
+        std::cout<<red
+                <<std::format("  {}:{}: unexpected token '{}'\n",
                                  e.lineNumber, e.columnNumber, e.unidentifiedToken)
-                  << reset;
+                <<reset;
     }
 }
 
 
 void Lexer::ErrorRecovery(Token &token, std::streampos lastAcceptedTokenPos,
                           TokenType lastAcceptedTokenType) {
-  if (lastAcceptedTokenPos == std::streampos(-1)) {
+  if(lastAcceptedTokenPos == std::streampos(-1)) {
     errors.emplace_back(token.GetLineNumber(), token.GetColumnNumber(),
                         token.GetLexeme());
-  } else {
+  } 
+  else{
     while (inputFileStream.tellg() > lastAcceptedTokenPos) {
       token.pop();
       inputFileStream.unget();
@@ -222,7 +224,8 @@ const std::vector<Token> &Lexer::GenerateTokens() {
           lastAcceptedTokenType = TokenType::ELLIPSIS;
           tokens.push_back(token);
           continue;
-        } else {
+        } 
+        else {
           // Careful review required
           ErrorRecovery(token, lastAcceptedTokenPos, lastAcceptedTokenType);
           continue;
@@ -263,14 +266,16 @@ const std::vector<Token> &Lexer::GenerateTokens() {
           lastAcceptedTokenPos = inputFileStream.tellg();
           lastAcceptedTokenType = TokenType::INCREMENT_OPERATOR;
           tokens.push_back(token);
-      } else if(c == '=') {
+      } 
+      else if(c == '=') {
           token.push(c);
           currentColumnNumber++;
           token.SetType(TokenType::COMPOUND_SUM);
           lastAcceptedTokenPos = inputFileStream.tellg();
           lastAcceptedTokenType = TokenType::COMPOUND_SUM;
           tokens.push_back(token);
-      } else {
+      } 
+      else {
           ErrorRecovery(token, lastAcceptedTokenPos, lastAcceptedTokenType);
       }
       continue;
