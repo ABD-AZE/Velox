ea7cf841f2c5cd42791848e140360a50bc5c142e
Abdullah Azeem <abdullahazeem770@gmail.com>
2025-08-09 15:07:29 +0530

    modified lexer.cpp

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -1,1 +1,25 @@
-#include "lexer.hh"
+#include "lexer.hpp"
+
+Lexer::Lexer(const std::string &file) : inputFileStream(file) {
+  inputFileStream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  if (!inputFileStream.is_open()) {
+    throw std::runtime_error("Failed to open file: " + file);
+  }
+}
+
+void Lexer::PrintSymbolTable() const {
+  // unimplemented
+}
+
+void Lexer::PrintTokens() const {
+  // add pretty printing of tokens here
+}
+
+void Lexer::PrintErrors() const {
+  // add pretty printing of errors here
+}
+
+const std::vector<Token> &Lexer::GenerateTokens() {
+  // main logic goes here
+  return tokens;
+}
