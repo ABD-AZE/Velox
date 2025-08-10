43c62f8c53d59c746714b5750fe973b8de6f60f5
Nisarg Prajapati <147189996+Munnond@users.noreply.github.com>
2025-08-10 22:44:31 +0530

    Update test7.c

--- tests/test7.c
+++ tests/test7.c
@@ -1,5 +1,6 @@
 "a" "hello" "quote: \" slash: \\ newline:\n tab:\t"
 'c' '\n' '\t' '\'' '\"' '\\'
 "a
-"        // this will trigger error recovery on unterminated string (newline)
-'a        // this will trigger error recovery on unterminated char (EOF or newline)
+"       
+'a        
+
