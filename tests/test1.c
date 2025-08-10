0d3117666defe670513651dee4a9d4a88f654c72
Nisarg Prajapati <147189996+Munnond@users.noreply.github.com>
2025-08-10 22:31:55 +0530

    Update test1.c

--- tests/test1.c
+++ tests/test1.c
@@ -1,1 +1,6 @@
-savdsgvsdfgdfv /asfvdsvsvg
+int main
+return
+while loopVar for if else do break continue
+static extern unsigned long signed double char sizeof struct enum union typedef goto switch case default
+printf scanf
+alpha beta123 gamma deltaEpsilon
